6ec5a4522da6856aee5b9051d658d7ae0863d5f8
Francesco3456 <francesco.plpl03@gmail.com>
2024-02-29 15:17:17 +0100

    struttura log modificata

--- calcio/src/main/java/Application/App.java
+++ calcio/src/main/java/Application/App.java
@@ -1,15 +1,6 @@
 package Application;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.config.Configurator;
-
-import utils.logger.InformazioniLogger;
-
-
 public class App {
-
-    private static final Logger logger = LogManager.getLogger(InformazioniLogger.class);
 
     public static void main(String[] args) {
     	
--- calcio/src/main/java/utils/logger/InformazioniLogger.java
+++ calcio/src/main/java/utils/logger/InformazioniLogger.java
@@ -6,6 +6,26 @@ import org.apache.logging.log4j.Logger;
 
 public class InformazioniLogger {
 	
-	public static final Logger logger = LogManager.getLogger(InformazioniLogger.class);
+	private static final Logger logger = LogManager.getLogger(InformazioniLogger.class);
+	
+	public void getLogInfo() {
+		logger.info("log di info");
+	}
+	
+	public void getLogDebug() {
+		logger.debug("log di debug");
+	}
+	
+	public void getLogWarn() {
+		logger.warn("log di warn");
+	}
+	
+	public void getLogError() {
+		logger.error("log di error");
+	}
+	
+	public void getLogFatal() {
+		logger.fatal("log di fatal");
+	}
 	
 }
