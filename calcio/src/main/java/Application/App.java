e09145b56364587b287f73963ed9d15ca0f11d88
Francesco3456 <francesco.plpl03@gmail.com>
2024-02-29 15:02:48 +0100

    aggiunta log

--- calcio/src/main/java/Application/App.java
+++ calcio/src/main/java/Application/App.java
@@ -1,10 +1,18 @@
 package Application;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+
+import utils.logger.InformazioniLogger;
+
 
 public class App {
 
-	public static void main(String[] args) {
-		
-	}
+    private static final Logger logger = LogManager.getLogger(InformazioniLogger.class);
+
+    public static void main(String[] args) {
+    	
+    }
 
 }
---
+++ calcio/src/main/java/utils/logger/InformazioniLogger.java
@@ -0,0 +1,11 @@
+package utils.logger;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class InformazioniLogger {
+	
+	public static final Logger logger = LogManager.getLogger(InformazioniLogger.class);
+	
+}
